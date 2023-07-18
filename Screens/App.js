a20e92deec2e2cad6a925a319f485136236cc0db
valeri143 <valeria23200523251605@gmail.com>
2023-07-18 15:30:22 +0300

    added arrow

--- Screens/App.js
+++ Screens/App.js
@@ -4,11 +4,6 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { Home } from "./src/components/Home/Home";
 import { LoginScreen } from "./src/components/EnterScreens/LoginScreen/LoginScreen";
 import { RegistrationScreen } from "./src/components/EnterScreens/RegistrationScreen/RegistrationScreen";
-import { PostsScreen } from "./src/components/PostsScreen/PostsScreen";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { CreatePostsScreen } from "./src/components/CreatePostsScreen/CreatePostsScreen";
-import { ProfileScreen } from "./src/components/ProfileScreen/ProfileScreen";
-
 
 export default function App() {
   const MainStack = createStackNavigator();
@@ -23,7 +18,7 @@ export default function App() {
   <MainStack.Screen name="Login" options={{title:"", headerStyle: {
         height:0
       }}} component={LoginScreen} />
-  <MainStack.Screen name="Home" options={{title:"", headerStyle:{height: 0}
+  <MainStack.Screen name="Home" options={{title:"", headerLeft: null,headerStyle:{height: 0}
       }} component={Home} />
 </MainStack.Navigator>
 </NavigationContainer>
--- Screens/src/components/Home/Home.jsx
+++ Screens/src/components/Home/Home.jsx
@@ -6,29 +6,14 @@ import logOut from "../../images/logout.png";
 import { Pressable, Image } from "react-native";
 import { useEffect} from 'react';
 import { Ionicons } from "@expo/vector-icons";
-import React, { useState } from 'react';
-
-
-import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
 
 const Tabs = createBottomTabNavigator();
 
-export const Home = ({ navigation, route }) => {
-  const [isActive, setIsActive] = useState(false);
+export const Home = ({ navigation }) => {
 
   useEffect(() => {
     navigation.navigate('PostsScreen');
   }, []);
-
-  const routeName = getFocusedRouteNameFromRoute(route);
-
-  useEffect(() => {
-    if (routeName === 'ProfileScreen') {
-      setIsActive(true);
-    } else {
-      setIsActive(false);
-    }
-  }, [routeName]);
 
   return (
     <Tabs.Navigator
@@ -39,10 +24,10 @@ export const Home = ({ navigation, route }) => {
           if (route.name === 'PostsScreen') {
             iconName = 'grid-outline';
           } else if (route.name === 'CreatePostsScreen') {
-            iconName = isActive ? 'person-outline' : 'add-outline';
-            color = isActive ? '#fff' : '#fff';
+            iconName = 'add-outline';
+            color = '#fff';
           } else if (route.name === 'ProfileScreen') {
-            iconName = focused ? 'add-outline' : 'person-outline';
+            iconName = 'person-outline';
           }
           return <Ionicons name={iconName} size={size} color={color} />;
         },
@@ -51,17 +36,11 @@ export const Home = ({ navigation, route }) => {
                 activeTintColor: "gray",
                 inactiveTintColor: "gray",
               }}
-              onTabPress={({ route }) => {
-                if (route.name === 'ProfileScreen') {
-                  setIsActive(true);
-                }else {
-                  setIsActive(false)
-                }
-              }}
     >
        <Tabs.Screen name="PostsScreen" options={{title:"Публікації",headerStyle:{height:100}, headerTitleStyle: {
          marginTop:50
-            },tabBarLabelStyle: { display:"none"},tabBarActiveTintColor: "gray",
+            },
+            tabBarLabelStyle: { display:"none"},tabBarActiveTintColor: "gray",
             headerRight: () => (
              <Pressable
                onPress={() => alert("You have just loged out!")}
@@ -73,9 +52,102 @@ export const Home = ({ navigation, route }) => {
             headerStyle:{height:100},
             headerTitleStyle: {marginTop: 50},
             tabBarStyle:{display:"none"} ,
+            headerLeft: () => (
+              <Pressable onPress={() => navigation.navigate("PostsScreen")}>
+                 <Ionicons name="arrow-back" size={24} color="gray" style={{marginLeft: 20, marginTop:50}} />
+              </Pressable>
+             
+            ),
               tabBarLabelStyle: { display:"none"}, tabBarIconStyle:{backgroundColor:"#FF6C00", width:70, height:40,marginTop:8, borderRadius:20}
          }} component={CreatePostsScreen}/>
           <Tabs.Screen name="ProfileScreen" options={{title:"",headerStyle:{height:0}, tabBarLabelStyle: { display:"none"}}} component={ProfileScreen}/>
     </Tabs.Navigator>
   );
 };
+
+
+
+
+
+
+// import { PostsScreen } from "../PostsScreen/PostsScreen";
+// import { CreatePostsScreen } from "../CreatePostsScreen/CreatePostsScreen";
+// import { ProfileScreen } from "../ProfileScreen/ProfileScreen";
+// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+// import logOut from "../../images/logout.png";
+// import { Pressable, Image } from "react-native";
+// import { useEffect} from 'react';
+// import { Ionicons } from "@expo/vector-icons";
+// import React, { useState } from 'react';
+
+
+// import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
+
+// const Tabs = createBottomTabNavigator();
+
+// export const Home = ({ navigation, route }) => {
+//   const [isActive, setIsActive] = useState(false);
+
+//   useEffect(() => {
+//     navigation.navigate('PostsScreen');
+//   }, []);
+
+//   const routeName = getFocusedRouteNameFromRoute(route);
+
+//   useEffect(() => {
+//     if (routeName === 'ProfileScreen') {
+//       setIsActive(true);
+//     } else {
+//       setIsActive(false);
+//     }
+//   }, [routeName]);
+
+//   return (
+//     <Tabs.Navigator
+//       screenOptions={({ route }) => ({
+//         tabBarIcon: ({ focused, color, size }) => {
+//           let iconName;
+
+//           if (route.name === 'PostsScreen') {
+//             iconName = 'grid-outline';
+//           } else if (route.name === 'CreatePostsScreen') {
+//             iconName = isActive ? 'person-outline' : 'add-outline';
+//             color = isActive ? '#fff' : '#fff';
+//           } else if (route.name === 'ProfileScreen') {
+//             iconName = focused ? 'add-outline' : 'person-outline';
+//           }
+//           return <Ionicons name={iconName} size={size} color={color} />;
+//         },
+//       })}
+//       tabBarOptions={{
+//                 activeTintColor: "gray",
+//                 inactiveTintColor: "gray",
+//               }}
+//               onTabPress={({ route }) => {
+//                 if (route.name === 'ProfileScreen') {
+//                   setIsActive(true);
+//                 }else {
+//                   setIsActive(false)
+//                 }
+//               }}
+//     >
+//        <Tabs.Screen name="PostsScreen" options={{title:"Публікації",headerStyle:{height:100}, headerTitleStyle: {
+//          marginTop:50
+//             },tabBarLabelStyle: { display:"none"},tabBarActiveTintColor: "gray",
+//             headerRight: () => (
+//              <Pressable
+//                onPress={() => alert("You have just loged out!")}
+//               >
+//                 <Image source={logOut} style={{marginRight: 16, marginTop:50}}/>
+//               </Pressable>)}} component={PostsScreen} />
+//           <Tabs.Screen name="CreatePostsScreen" options={{
+//             title:"Створити публікацію",
+//             headerStyle:{height:100},
+//             headerTitleStyle: {marginTop: 50},
+//             tabBarStyle:{display:"none"} ,
+//               tabBarLabelStyle: { display:"none"}, tabBarIconStyle:{backgroundColor:"#FF6C00", width:70, height:40,marginTop:8, borderRadius:20}
+//          }} component={CreatePostsScreen}/>
+//           <Tabs.Screen name="ProfileScreen" options={{title:"",headerStyle:{height:0}, tabBarLabelStyle: { display:"none"}}} component={ProfileScreen}/>
+//     </Tabs.Navigator>
+//   );
+// };
